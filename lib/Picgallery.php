f264e0cdf650709932fa7547ddf3fc98f8172707
Antonio Haley <antonio.haley@gmail.com>
2012-10-06 16:16:49 -0400

    Removed unused requires from root file

--- lib/Picgallery.php
+++ lib/Picgallery.php
@@ -1,11 +1,9 @@
 <?php
 
 require_once 'Picgallery/Picgallery.php';
-require_once 'Picgallery/DropboxAdapter.php';
 require_once 'Picgallery/GoogleSession.php';
 require_once 'Picgallery/AuthSubGoogleSession.php';
 require_once 'Picgallery/ClientLoginGoogleSession.php';
-require_once 'Picgallery/PicasaRepository.php';
 require_once 'Picgallery/LocalImageRepository.php';
 require_once 'Picgallery/AlbumRepository.php';
 require_once 'Picgallery/ImageSyncer.php';
