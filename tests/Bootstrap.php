49d74ee1de99b51498a45ecfe672ab2381107133
Antonio Haley <antonio.haley@gmail.com>
2012-11-14 00:00:23 +0100

    Pulling google user from environment var

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -2,7 +2,7 @@
 
 define('ROOT_PATH', dirname(__DIR__));
 define('LIBRARY_PATH', realpath(ROOT_PATH . '/lib'));
-define('GOOGLE_USER', 'antonio.haley@gmail.com');
+define('GOOGLE_USER', getenv['GOOGLE_USER']);
 
 $include_path = explode(':', get_include_path());
 $include_path[] = LIBRARY_PATH;
