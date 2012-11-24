a9ccaf697318a611068e75b7ec98b45fab57e846
Antonio Haley <antonio.haley@gmail.com>
2012-11-24 18:32:58 +0100

    Added message if GOOGLE_USER is not set.

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -2,7 +2,19 @@
 
 define('ROOT_PATH', dirname(__DIR__));
 define('LIBRARY_PATH', realpath(ROOT_PATH . '/lib'));
+$google_user = getenv('GOOGLE_USER');
+if (!$google_user) {
+    echo <<<EOT
+
+Please set the environment variable GOOGLE_USER if you with to run
+functional tests.
+
+
+EOT;
+}
+else {
 define('GOOGLE_USER', getenv('GOOGLE_USER'));
+}
 
 $include_path = explode(':', get_include_path());
 $include_path[] = LIBRARY_PATH;
