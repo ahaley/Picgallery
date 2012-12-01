500f3d46d0669a5302019f126fa8fb77ad70daae
Antonio Haley <antonio.haley@gmail.com>
2012-12-01 18:39:09 +0100

    Simplified Image.php
    The backing store is not going to reference Image by id, rather it's going to
    be a composite key of gallery/name. Added the gallery property.

--- lib/Picgallery/Image.php
+++ lib/Picgallery/Image.php
@@ -4,20 +4,20 @@ namespace Picgallery;
 
 class Image
 {
-    private $id;
     private $name;
-    private $thumbnail;
+    private $gallery;
+    private $thumbnail_url;
     private $url;
     private $size;
     
-    public function setId($id) { $this->id = $id; }
-    public function getId() { return $this->id; }
     public function setName($name) { $this->name = $name; }
     public function getName() { return $this->name; }
-    public function setThumbnailUrl($thumbnail) { $this->thumbnail = $thumbnail; }
+    public function setGallery($gallery) { $this->gallery = $gallery; }
+    public function getGallery() { return $this->gallery; }
+    public function setThumbnailUrl($thumbnail) { 
+        $this->thumbnail_url = $thumbnail_url;
+    }
     public function getThumbnailUrl() { return $this->thumbnail; }
     public function setUrl($url) { $this->url = $url; }
     public function getUrl() { return $this->url; }
-    public function setSize($size) { $this->size = $size; }
-    public function getSize() { return $this->size; }
 }
