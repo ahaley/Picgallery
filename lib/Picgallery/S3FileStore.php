81ebff9ecf569900b2d1b89fda743ae4cca32e23
Antonio Haley <antonio.haley@gmail.com>
2013-02-10 19:13:10 +0100

    Added S3 file store source

---
+++ lib/Picgallery/S3FileStore.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Picgallery;
+
+use Aws\S3\S3Client;
+use Aws\S3\Enum\CannedAcl;
+use Aws\Common\Enum\Region;
+
+class S3FileStore implements FileStoreInterface
+{
+    private $client;
+    private $bucketName;
+    private $localPath;
+    private $urlPath;
+
+    public function __construct($bucketName = 'picgallery_bucket', $localPath, $urlPath)
+    {
+        $this->bucketName = $bucketName;
+        $this->localPath = $localPath;
+        $this->urlPath = $urlPath;
+    }
+
+    public function fileExists($filename)
+    {
+    }
+    
+    public function uploadFile($filename, $path)
+    {
+        $client = $this->getClient();
+        $fp = fopen($path, "r");
+    
+        $model = $client->putObject(array(
+            'Bucket' => $this->bucketName,
+            'Key' => $this->localPath . '/' . $filename,
+            'Body' => $fp,
+            'ACL' => CannedAcl::PUBLIC_READ,
+        ));        
+        
+        fclose($fp);
+    }
+    
+    public function getUrl($filename)
+    {
+        return $this->urlPath . '/' . $filename;
+    }
+    
+    public function getFileSize($filename)
+    {
+    }
+    
+    public function removeFile($filename)
+    {
+        $client = $this->getClient();
+        $response = $client->deleteObject(array(
+            'Bucket' => $this->bucketName,
+            'Key' => $this->localPath . '/' . $filename
+        ));
+    }
+    
+    public function listFiles()
+    {
+    }
+    
+    private function getClient()
+    {
+        if ($this->client === NULL) {
+            $this->client = S3Client::factory(array(
+                'key' => AWS_KEY,
+                'secret' => AWS_SECRET,
+                'region' => Region::US_EAST_1
+            ));
+            
+            $this->client->createBucket(array(
+                'Bucket' => $this->bucketName
+            ));
+        }
+
+        return $this->client;
+    }
+}
---
+++ tests/integration/S3FileStoreTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace tests\integration;
+
+use Picgallery\S3FileStore;
+
+require_once LIBRARY_PATH . '/config.php';
+
+class S3FileStoreTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @covers \Picgallery\S3FileStore::upload
+     */
+    public function ShouldUploadFile()
+    {
+        $store = new S3FileStore('picgallery_tests', 'integration', '');
+        $store->uploadFile('test2', '/Users/ahaley/test');
+    }
+    
+    /**
+     * @test
+     * @covers \Picgallery\S3FileStore::remove
+     */
+    public function ShouldRemoveFile()
+    {
+        $store = new S3FileStore('picgallery_tests', 'integration', '');
+        $store->removeFile('test2');
+    }
+}
